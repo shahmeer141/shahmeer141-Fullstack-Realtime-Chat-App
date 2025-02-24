08d8cf9534aad423bf642b63b0758498b9461096
Shahmeer Nadeem <shahmeersiddique296@gmail.com>
2025-02-24 22:51:38 +0500

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,7 @@ const __dirname = path.resolve()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"https://shahmeer141-fullstack-realtime-chat-app-frontend.vercel.app/",
+    origin:"http://localhost:5173",
     credentials: true,
 }))
 
