5469716a3c7dcdb2dcf33dcfbe7f737b1d215835
Shahmeer Nadeem <shahmeersiddique296@gmail.com>
2025-02-24 22:52:45 +0500

    Update useAuthStore.js

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from './../lib/axios.js';
 import toast from "react-hot-toast";
 import { io } from "socket.io-client"
 
-const BASE_URL = import.meta.env.MODE === "development" ? "https://shahmeer141-fullstack-realtime-chat-app.vercel.app/" : "/"
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5173" : "/"
 
 
 export const useAuthStore = create((set, get)=>({
