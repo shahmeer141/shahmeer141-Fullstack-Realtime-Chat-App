0a3cde7be1a7ef717ffde9341dfc6f66c4be56e2
Shahmeer Nadeem <shahmeersiddique296@gmail.com>
2025-02-24 18:42:23 +0500

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
+    baseURL: import.meta.env.MODE === "development" ? "shahmeer141-fullstack-realtime-chat-app.vercel.app" : "/api",
     withCredentials: true,
 })
